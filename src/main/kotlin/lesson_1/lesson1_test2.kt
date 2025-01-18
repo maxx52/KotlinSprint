801e41a81569c31b149130946608b0b9b30e89ba
gospelwork <gospelwork30@gmail.com>
2025-01-18 07:59:12 +0700

    KS-1-1 complete test 2

---
+++ src/main/kotlin/lesson_1/lesson1_test2.kt
@@ -0,0 +1,22 @@
+package ru.maxx52.lesson_1
+
+fun main() {
+    println("Задача 2 к уроку 1")
+
+    // Кол-во заказов
+    val amountOfOrders: Int = 75
+
+    // Текст благодарности
+    val thanksForBuy: String = "Благодарим Вас за покупку и ждём Вас снова!"
+
+    println("Количество заказов: $amountOfOrders")
+    println(thanksForBuy)
+
+    // Кол-во работников магазина
+//    val countOfWorkers: Int = 2000
+//    println("Текущее количество работников: $countOfWorkers человек")
+
+    // Кол-во работников магазина (с учётом увольнения 1 работника)
+    val countOfWorkers: Int = 1999
+    println("Текущее количество работников: $countOfWorkers человек")
+}
