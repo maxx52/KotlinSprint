85a289911955aed6eaa48b70a74e21e9369c063c
gospelwork <gospelwork30@gmail.com>
2025-01-18 07:38:08 +0700

    KS-1-1 complete exercise 1

---
+++ src/main/kotlin/lesson_1/lesson1_ex1.kt
@@ -0,0 +1,11 @@
+package ru.maxx52.lesson_1
+
+fun main() {
+    println("Задача 1 урок 1")
+
+    // Кол-во заказов
+    val amountOfOrders: Int = 75
+
+    // Текст благодарности
+    val thanksForBuy: String = "Благодарим Вас за покупку и ждём Вас снова!"
+}
--- src/main/kotlin/lesson_1/lesson_1.kt
+++
@@ -1,5 +0,0 @@
-package ru.maxx52.lesson_1
-
-fun main() {
-    println("Задача 1 урок 1")
-}
