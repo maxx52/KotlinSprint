856f8b76e933f5807ca51c1eee9cb151a59b7991
gospelwork <gospelwork30@gmail.com>
2025-01-18 08:24:48 +0700

    KS-1-1 complete test 3

---
+++ src/main/kotlin/lesson_1/lesson1_test3.kt
@@ -0,0 +1,26 @@
+package ru.maxx52.lesson_1
+
+fun main() {
+
+    println("Задача 3 к Уроку 1")
+
+    // Год полёта в космос Ю.Гагарина
+    val year: Int = 1961
+
+    // Время старта ракеты
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    // Время посадки капсулы Ю.Гагарина
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+
+}
