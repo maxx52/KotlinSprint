fe6f1ea5a9ff95dbc2a66d0e73874a25c8a41b8a
gospelwork <gospelwork30@gmail.com>
2025-01-19 17:18:51 +0700

    KS-1-5 complete test 5*

--- src/main/kotlin/lesson_1/lesson1_test5.kt
+++ src/main/kotlin/lesson_1/lesson1_test5.kt
@@ -4,32 +4,33 @@ fun main() {
 
     // Объявляем переменную типа Double,
     // т.к. при дальнейшем делении не всегда результат будет целочисленным
-    val seconds: Double = 6480.0
+    val seconds = 6480
 
     // Получаем кол-во часов и записываем в новую переменную:
-    val hours = (seconds / 3600).toInt()
+    val hours = seconds / SECONDS_IN_HOUR
 
-    // Получаем кол-во минут и приводим к целому числу
-    val minutes = (seconds % 3600 / 60).toInt()
+    // Получаем кол-во минут
+    val minutes = seconds % SECONDS_IN_HOUR / MINUTE_IN_HOUR
 
-    // Вычисляем кол-во ставшихся секунд
-    val sec = seconds.toInt() - (hours * 3600 + minutes * 60)
+    // Вычисляем кол-во секунд
+    val sec = seconds % SECONDS_IN_HOUR % MINUTE_IN_HOUR
+
+    println(sec)
 
     // Проверка разрядности полученных данных
-    val nullString: String = "0"
     var result: String
     if (hours < 10) {
-        result = "$nullString$hours:"
+        result = String.format("%02d:", hours)
     } else {
         result = "$hours:"
     }
     if (minutes < 10) {
-        result += "$nullString$minutes:"
+        result += String.format("%02d:", minutes)
     } else {
         result += "$minutes:"
     }
     if (sec < 10) {
-        result += "$nullString$sec"
+        result += String.format("%02d:", sec)
     } else {
         result += "$sec"
     }
@@ -38,3 +39,7 @@ fun main() {
     println(result)
 
 }
+
+// Объявляем константы, чтобы избежать путаницы в программе
+const val SECONDS_IN_HOUR = 3600
+const val MINUTE_IN_HOUR = 60
