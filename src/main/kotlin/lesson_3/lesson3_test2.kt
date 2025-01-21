39b577965bcaba213992ae561e66f7545dd4ea19
gospelwork <gospelwork30@gmail.com>
2025-01-21 21:13:21 +0700

    KS-3-2 error correction

--- src/main/kotlin/lesson_3/lesson3_test2.kt
+++ src/main/kotlin/lesson_3/lesson3_test2.kt
@@ -3,12 +3,16 @@ package ru.maxx52.lesson_3
 fun main() {
 
     // Исходные данные:
-    val nameBefore = "Татьяна Сергеевна Андреева"
-    val nameAfter = "Татьяна Сергеевна Сидорова"
-    val ageBefore = 20
-    val ageAfter = 22
+    val name = "Татьяна"
+    val fatherName = "Сергеевна"
+    var surName = "Андреева"
+    var age = 20
 
-    // Вывод результатов:
-    println("$nameBefore, %d лет".format(ageBefore))
-    println("$nameAfter, %d лет".format(ageAfter))
+    // Вывод результатов до замужества, в 20 лет:
+    println("%s %s %s, %d лет".format(surName, name, fatherName, age))
+
+    // В 22 года вышла замуж:
+    surName = "Сидорова"
+    age = 22
+    println("%s %s %s, %d лет".format(surName, name, fatherName, age))
 }
