9329032b9256bfe5a05cbe5501b3d3c2fb429da1
gospelwork <gospelwork30@gmail.com>
2025-01-23 21:38:31 +0700

    KS-4-2 error correction

--- src/main/kotlin/lesson_4/lesson4_test2.kt
+++ src/main/kotlin/lesson_4/lesson4_test2.kt
@@ -3,26 +3,41 @@ package ru.maxx52.lesson_4
 fun main() {
 
     // Исходные данные:
-    val averageWeight = 35
-    val averageVolume = 100
     val weightFirst = 20
     val weightSecond = 50
     val volumeFirst = 80
     val volumeSecond = 100
 
     // Соответствие параметров первого груза:
-    val comparisonFirst = weightFirst > averageWeight && volumeFirst < averageVolume
+    val comparisonFirst = weightFirst > AVERAGE_WEIGHT && volumeFirst < AVERAGE_VOLUME
 
     // Соответствие параметров второго груза:
-    val comparisonSecond = weightSecond > averageWeight && volumeSecond < averageVolume
+    val comparisonSecond = weightSecond > AVERAGE_WEIGHT && volumeSecond < AVERAGE_VOLUME
 
     // Вывод сообщения:
     println("Груз с весом $weightFirst кг и объемом $volumeFirst л соответствует категории 'Average': $comparisonFirst")
     println("Груз с весом $weightSecond кг и объемом $volumeSecond л соответствует категории 'Average': $comparisonSecond")
 
     // Второй вариант вывода сообщения:
-    println("Груз с весом $weightFirst кг и объемом $volumeFirst л соответствует категории 'Average': ${weightFirst > averageWeight && volumeFirst < averageVolume}")
-    println("Груз с весом $weightSecond кг и объемом $volumeSecond л соответствует категории 'Average': ${weightSecond > averageWeight && volumeSecond < averageVolume}")
-
-
+    val first = weightFirst > AVERAGE_WEIGHT && volumeFirst < AVERAGE_VOLUME
+    val second = weightSecond > AVERAGE_WEIGHT && volumeSecond < AVERAGE_VOLUME
+    println(buildString {
+        append("Груз с весом ")
+        append(weightFirst)
+        append(" кг и объемом ")
+        append(volumeFirst)
+        append(" л соответствует категории 'Average': ")
+        append(first)
+    })
+    println(buildString {
+        append("Груз с весом ")
+        append(weightSecond)
+        append(" кг и объемом ")
+        append(volumeSecond)
+        append(" л соответствует категории 'Average': ")
+        append(second)
+    })
 }
+// Входные данные выносим в константы:
+const val AVERAGE_WEIGHT = 35
+const val AVERAGE_VOLUME = 100
