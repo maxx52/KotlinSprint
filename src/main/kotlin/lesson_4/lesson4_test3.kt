b4c55cd7adae5e93b528d43014ff37c476408ee5
gospelwork <gospelwork30@gmail.com>
2025-01-23 21:53:22 +0700

    KS-4-3 error correction

--- src/main/kotlin/lesson_4/lesson4_test3.kt
+++ src/main/kotlin/lesson_4/lesson4_test3.kt
@@ -2,24 +2,23 @@ package ru.maxx52.lesson_4
 
 fun main() {
 
-    // Исходные данные:
-    val weather = true
-    val tent = true
-    val humidity = 20
-    val season = "winter"
-
     // Благоприятные условия:
     val comfortWeather = true
     val comfortTent = true
     val comfortHumidity = 20
-    val comfortSeason = "no winter"
+    val comfortSeason = "winter"
 
     // Сравнение текущих условий с благоприятными:
-    val result = (weather == comfortWeather) &&
-            (tent == comfortTent) && (humidity == comfortHumidity) &&
-            (season == comfortSeason)
+    val result = (IS_SUNNY == comfortWeather) &&
+            (IS_AWNING_OPEN == comfortTent) &&
+            (HUMIDITY == comfortHumidity) &&
+            (SEASON != comfortSeason)
 
     // Вывод результата:
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
-
 }
+// Исходные данные заносим в константы:
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
+const val HUMIDITY = 20
+const val SEASON = "winter"
