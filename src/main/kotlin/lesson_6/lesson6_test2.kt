e8a7c095ca881e1922c5b988e51000712f69f737
maxx52 <gospelwork@yandex.ru>
2025-01-31 23:42:52 +0700

    KS-6-2 clean up code, error correction

--- src/main/kotlin/lesson_6/lesson6_test2.kt
+++ src/main/kotlin/lesson_6/lesson6_test2.kt
@@ -4,28 +4,20 @@ fun main() {
 
     println("Таймер")
 
-    // Запрашиваем у пользователя кол-во секунд:
     println("Введите количество секунд для обратного отсчёта:")
     val secondsInput = readln()
     do {
         try {
-            // Проверка числа на положительность:
             var seconds = secondsInput.toInt()
             if (seconds <= 0) {
                 println("Введите положительное число больше нуля")
             } else {
-                // Включаем обратный отсчет:
-                while (seconds > 0) {
-                    seconds--
-                    Thread.sleep(MILLIS)
-                }
-                // По окончании времени выводим сообщение:
+                Thread.sleep(seconds * MILLIS)
                 println("Прошло $secondsInput сек")
             }
         } catch (e: NumberFormatException) {
             println("Введите корректное целое число!")
         }
-    } while (secondsInput == null) // если пользователь ничего не ввел, выходим из программы
+    } while (secondsInput == null)
 }
-// Кол-во миллисекунд в секунде:
 const val MILLIS = 1000L
