ac783329e155b0fefc8587d65019db2592aed172
maxx52 <gospelwork@yandex.ru>
2025-02-01 22:47:59 +0700

    KS-7-4 Password generator Advanced

---
+++ src/main/kotlin/lesson_7/lesson7_test5.kt
@@ -0,0 +1,29 @@
+package ru.maxx52.lesson_7
+
+fun main() {
+
+    println("Password generator Advanced")
+
+    val numbers = 0..9
+    val lettersLowercase = 'a'..'z'
+    val lettersUppercase = 'A'..'Z'
+    val sharedArray = (numbers + lettersLowercase + lettersUppercase)
+    val password = StringBuilder()
+
+    try {
+        println("Введите требуемое количество символов, минимум 6:")
+        val passwordLength = readln().toInt()
+
+        if (passwordLength < 6) {
+            println("Количество символов не может быть меньше 6")
+        } else {
+            for (i in 0 until  passwordLength) {
+                val randomSymbol = sharedArray.random()
+                password.append(randomSymbol)
+            }
+            println("Ваш пароль: $password")
+        }
+    } catch (e: NumberFormatException) {
+        println("Неверный формат числа!")
+    }
+}
