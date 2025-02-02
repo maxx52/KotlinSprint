fbee3c8cf63528a5117af13a10062b5bb0332fd1
maxx52 <gospelwork@yandex.ru>
2025-02-02 17:40:34 +0700

    KS-8-1 sum the elements in array

---
+++ src/main/kotlin/lesson_8/lesson8_test1.kt
@@ -0,0 +1,10 @@
+package ru.maxx52.lesson_8
+
+fun main() {
+
+    println("Просмотры рекламы")
+
+    val arrayOfVision = intArrayOf(145, 256, 223, 338, 159, 565, 478)
+
+    println("Просмотров рекламы за неделю: ${arrayOfVision.sum()}")
+}
