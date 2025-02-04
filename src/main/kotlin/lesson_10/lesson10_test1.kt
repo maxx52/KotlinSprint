5a757b938b1037b13a9fd6f609ee36b1d898759b
maxx52 <gospelwork@yandex.ru>
2025-02-04 22:14:31 +0700

    KS-10-1 game to dice

---
+++ src/main/kotlin/lesson_10/lesson10_test1.kt
@@ -0,0 +1,23 @@
+package ru.maxx52.lesson_10
+
+fun main() {
+
+    println("Игра в кости")
+
+    val userDice = rollDice()
+    val computerDice = rollDice()
+
+    println("Счет $userDice : $computerDice")
+
+    if (userDice > computerDice) {
+        println("Победило человечество!")
+    } else if (userDice < computerDice) {
+        println("Победила машина!")
+    } else {
+        println("Победила дружба!")
+    }
+}
+
+fun rollDice(): Int {
+    return (1..6).random()
+}
