474d4d3c70dc339e74131193f64519382bdf09f9
maxx52 <gospelwork@yandex.ru>
2025-02-04 21:10:15 +0700

    KS-9-2 fewer conditions

--- src/main/kotlin/lesson_9/lesson9_test2.kt
+++ src/main/kotlin/lesson_9/lesson9_test2.kt
@@ -6,25 +6,24 @@ fun main() {
     val ingredientList = mutableListOf("творог", "сахар", "изюм")
     println("В рецепте есть базовые ингредиенты: ${ingredientList.joinToString(", ")}")
 
-    println("Желаете добавить ещё? (да / нет)")
-    val permissionInput = readln().trim()
+    try {
+        println("Желаете добавить ещё? (да / нет)")
+        val permissionInput = readln().trim()
 
-    if (permissionInput.equals("нет", ignoreCase = true)) {
-        println("До свидания!")
-        return
-    } else if (permissionInput.equals("да", ignoreCase = true)) {
-        println("Какой ингредиент вы хотели бы добавить?")
-        val ingredientInput = readln().trim()
+        if (permissionInput.equals("да", ignoreCase = true)) {
+            println("Какой ингредиент вы хотели бы добавить?")
+            val ingredientInput = readln().trim()
 
-        if (ingredientInput.isBlank()) {
-            println("Название ингредиента не может быть пустым!")
-        } else if (ingredientList.contains(ingredientInput)) {
-            println("Ингредиент '$ingredientInput' уже есть в списке!")
+            if (ingredientList.contains(ingredientInput)) {
+                println("Ингредиент '$ingredientInput' уже есть в списке!")
+            } else {
+                ingredientList += ingredientInput
+                println("Теперь в рецепте есть следующие ингредиенты: ${ingredientList.joinToString(", ")}")
+            }
         } else {
-            ingredientList += ingredientInput
-            println("Теперь в рецепте есть следующие ингредиенты: ${ingredientList.joinToString(", ")}")
+            println("До свидания!")
         }
-    } else {
-        println("Неизвестный ответ! Пожалуйста, введите 'да' или 'нет'.")
+    } catch (e: NullPointerException) {
+        println("Название ингредиента не может быть пустым!")
     }
 }
