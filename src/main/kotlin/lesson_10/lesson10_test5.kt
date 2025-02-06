9c4dd80fd155b5660cd3b6fae52a74a7346caff3
maxx52 <gospelwork@yandex.ru>
2025-02-06 19:02:34 +0700

    KS-10-5 replace chars into the several Range, add const val

--- src/main/kotlin/lesson_10/lesson10_test5.kt
+++ src/main/kotlin/lesson_10/lesson10_test5.kt
@@ -2,6 +2,7 @@ package ru.maxx52.lesson_10
 
 const val USERNAME = "admin"
 const val PASSWORD = "password"
+const val TOKEN_LENGTH = 32
 
 fun main() {
     println("Добро пожаловать в интернет-магазин!")
@@ -35,14 +36,17 @@ fun userAuth(username: String, password: String): String? {
 }
 
 fun generateToken(): String {
-    val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    val charsUppercase = 'A'..'Z'
+    val charsLowercase = 'a'..'z'
+    val charsNumbers = '0'..'9'
+    val chars = charsUppercase + charsLowercase + charsNumbers
     return (1..32)
         .map { chars.random() }
         .joinToString("")
 }
 
 fun getCart(token: String): List<String>? {
-    return if (token.length == 32) {
+    return if (token.length == TOKEN_LENGTH) {
         listOf("Товар 1", "Товар 2", "Товар 3")
     } else {
         null
