c93c12545d3576c3f44fdc78ecdc6d06039938f4
maxx52 <gospelwork@yandex.ru>
2025-02-08 19:48:15 +0700

    KS-13-2 forgot change class name

--- src/main/kotlin/lesson_13/lesson13_test2.kt
+++ src/main/kotlin/lesson_13/lesson13_test2.kt
@@ -2,13 +2,13 @@ package ru.maxx52.lesson_13
 
 fun main() {
 
-    val contact1 = Contact("Alex", 89008007007)
-    val contact2 = Contact("Yana", 89091234567, "Reddit")
+    val contact1 = Contact1("Alex", 89008007007)
+    val contact2 = Contact1("Yana", 89091234567, "Reddit")
     contact1.printContact()
     contact2.printContact()
 }
 
-class Contact(
+class Contact1(
     private val name: String,
     private val phone: Long,
     private val company: String? = null
