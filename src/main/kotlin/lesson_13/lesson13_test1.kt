2af038c135d4e5484febcc107274efdea21e5de6
maxx52 <gospelwork@yandex.ru>
2025-02-09 21:42:54 +0700

    KS-13-1 remove initialization

--- src/main/kotlin/lesson_13/lesson13_test1.kt
+++ src/main/kotlin/lesson_13/lesson13_test1.kt
@@ -2,14 +2,14 @@ package ru.maxx52.lesson_13
 
 fun main() {
 
-    val contact1 = Contact("Alex", 89008007007)
+    val contact1 = Contact("Alex", 89008007007, null)
     val contact2 = Contact("Yana", 89091234567, "Kotlin")
 }
 
 class Contact(
     name: String,
     phone: Long,
-    company: String? = null
+    company: String?
 ) {
     init {
         println("Контакт: имя - $name, номер телефона - $phone, компания - ${company ?: "не указана"}")
