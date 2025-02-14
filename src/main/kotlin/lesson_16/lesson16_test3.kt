a846b4ff1e2fcaf804855bac38f31594da5d8a61
maxx52 <gospelwork@yandex.ru>
2025-02-14 16:52:41 +0700

    KS-16-3 validate user`s password

---
+++ src/main/kotlin/lesson_16/lesson16_test3.kt
@@ -0,0 +1,23 @@
+package ru.maxx52.lesson_16
+
+fun main() {
+
+    val user = User("Maxx")
+
+    println("Здравствуйте, ${user.login}, ведите пароль:")
+    try {
+        val passwordInput: String = readln().trim()
+        println("Правильность пароля: ${user.validatePassword(passwordInput)}")
+    } catch (e: NullPointerException) {
+        println("Пароль не должен быть пустым!")
+    }
+}
+
+class User(
+    val login: String,
+) {
+    private val password: String = "KotlinSprint"
+    fun validatePassword(passwordInput: String): Boolean {
+        return passwordInput == password
+    }
+}
