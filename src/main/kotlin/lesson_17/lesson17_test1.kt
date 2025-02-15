fc0aecf775f70f6c1cd3e17a7f25dcdf69ba98df
maxx52 <gospelwork@yandex.ru>
2025-02-15 21:59:51 +0700

    KS-17-1 modification code

--- src/main/kotlin/lesson_17/lesson17_test1.kt
+++ src/main/kotlin/lesson_17/lesson17_test1.kt
@@ -1,21 +1,24 @@
 package ru.maxx52.lesson_17
 
 fun main() {
-    val quiz = Quiz("Какой самый большой континент?", "Азия")
+    val quiz = Quiz()
 
     println(quiz.question)
     println("Правильный ответ: ${quiz.answer}")
 
     quiz.answer = "Евразия"
-    println("Обновленный ответ: ${quiz.answer}")
+    println("Правильный ответ: ${quiz.answer}")
 }
 
 class Quiz(
-    private val _question: String,
-    private var _answer: String
+    private var _question: String = "Какой самый большой континент?",
+    private var _answer: String = "Азия",
 ) {
-    val question: String
+    var question: String
         get() = _question
+        set(value) {
+            _question = value
+        }
 
     var answer: String
         get() = _answer
