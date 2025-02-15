5f10d4327367ed04311fbd2b5333dccb3efd2c81
maxx52 <gospelwork@yandex.ru>
2025-02-15 22:13:44 +0700

    KS-17-2 change realisation

--- src/main/kotlin/lesson_17/lesson17_test2.kt
+++ src/main/kotlin/lesson_17/lesson17_test2.kt
@@ -1,31 +1,27 @@
 package ru.maxx52.lesson_17
 
 fun main() {
-    val ship = SpaceShip("Galaxy", 700, "Andromeda")
+    val ship = SpaceShip()
 
     println(ship)
 
-    ship.changeName("NewGalaxy")
-    println(ship)
-
+    ship.name = "NewGalaxy"
     ship.speed = 750
     ship.port = "Milky Way"
     println("После изменений: $ship")
 }
 
 class SpaceShip(
-    private val _name: String,
-    var speed: Int,
-    var port: String
+    private val _name: String = "Galaxy",
+    var speed: Int = 700,
+    var port: String = "Andromeda",
 ) {
-    var name: String = ""
-        get() = _name
+    var name: String = _name
+        set(value) {
+            field = "$name - имя изменить нельзя!"
+        }
 
     override fun toString(): String {
         return "Корабль: $name, Средняя скорость: $speed, Порт приписки: $port"
-    }
-
-    fun changeName(newName: String) {
-        println("Имя корабля $name не может быть изменено")
     }
 }
