4053169340853ea81e854f29ae516cb85591df1f
maxx52 <gospelwork@yandex.ru>
2025-02-16 16:11:49 +0700

    KS-17-4 remove method and add setter

--- src/main/kotlin/lesson_17/lesson17_test4.kt
+++ src/main/kotlin/lesson_17/lesson17_test4.kt
@@ -4,29 +4,27 @@ fun main() {
     val delivery = Package(1)
 
     println("Текущая локация: ${delivery.currentLocation}")
-    delivery.replace(1)
+    delivery.currentLocation = Location.PLACE_B.description
     println("Текущая локация после замены: ${delivery.currentLocation}, кол-во перемещений ${delivery.moveCount}")
-    delivery.replace(2)
 }
 
 class Package(
     private val number: Int,
-    private var _location: String = Location.PLACE_A.description,
+    private var location: String = Location.PLACE_A.description,
 ) {
     var moveCount = 0
 
-    val currentLocation: String
-        get() = _location
-
-    fun replace(number: Int) {
-        if (number == this.number) {
-            _location = Location.PLACE_B.description
-            moveCount++
-            println("Посылка $number перемещена в $currentLocation")
-        } else {
-            println("Номер посылки не найден")
+    var currentLocation: String
+        get() = location
+        set(value) {
+            if (number == this.number) {
+                location = Location.PLACE_B.description
+                moveCount++
+                println("Посылка $number перемещена в $currentLocation")
+            } else {
+                println("Номер посылки не найден")
+            }
         }
-    }
 }
 
 enum class Location(val description: String) {
